4d359e591cb5d85185cce274baef1b11c75d01e5
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2020-12-13 16:13:49 +0100

    Add functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,55 @@
-use needletail::parse_fastx_file;
+use needletail::{parse_fastx_file,};
+use needletail::parser::{SequenceRecord, LineEnding, write_fastq};
+use std::fs::{File, OpenOptions};
+use std::io::{BufWriter};
+use std::process::exit;
 
-pub fn foo(path: &str) -> anyhow::Result<()> {
-    let mut reader = parse_fastx_file(path)?;
-    let mut n: usize = 0;
-    let mut slen: usize = 0;
+pub fn foo(path: &str, outpath: &str, minqual: f64, minlen: usize, maxlen: usize, offset: u8) {
+    let mut reader = parse_fastx_file(path).expect("Failed to open parser");
+    let mut writer = BufWriter::new(
+        match OpenOptions::new().write(true).create_new(true).open(outpath) {
+            Ok(file) => file,
+            Err(e) => {
+                eprintln!("Error opening new file: {}", e);
+                exit(1);
+            }
+        }
+    );
+    
     while let Some(maybe_record) = reader.next() {
-        let record = maybe_record?;
-        n += 1;
-        slen += record.seq().len();
+        let record = maybe_record.expect("Failed to parse record");
+        if passes(&record, minqual, minlen, maxlen, offset) {
+            write_fastq_record(&mut writer, &record)
+        }
     }
-    println!("{} {}", n, slen);
-    Ok(())
+}
+
+fn passes(rec: &SequenceRecord, min_qual: f64, 
+            min_len: usize, max_len: usize, offset: u8) -> bool {
+    if mean_phred_qual(rec, offset) < min_qual { return false }
+    if rec.seq().len() < min_len || rec.seq().len() > max_len { return false }
+    return true
+}
+
+fn phred_to_prob(phred: u8, offset: u8) -> f64 {
+    (10.0_f64).powf((phred - offset) as f64 / -10.0)
+}
+
+fn prob_to_phred(prob: f64) -> f64 {
+    prob.log10() * -10.0_f64
+}
+
+fn mean_phred_qual(rec: &SequenceRecord, offset: u8) -> f64 {
+    let mut n: f64 = 0.0;
+    let mut prob: f64 = 0.0;
+    for phred in rec.qual().unwrap() {
+        n += 1.0;
+        prob += phred_to_prob(*phred, offset);
+    }
+    prob_to_phred(prob / n)
+}
+    
+fn write_fastq_record(writer: &mut BufWriter<File>, rec: &SequenceRecord) {
+    write_fastq(rec.id(), &rec.seq(), rec.qual(),
+                writer, LineEnding::Unix).expect("Failed to write record.")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
 use nanofilt::*;
 use std::env;
 use std::process::exit;
+use std::str::FromStr;
 
 fn main() {
-    let path = match env::args().nth(1) {
-        Some(path) => path,
-        None => {
-            eprintln!("Usage: nanofilt <input>");
+    let config = Config::new();
+    foo(&config.inpath, &config.outpath, config.min_qual, config.min_len, config.max_len, config.phred_offset)
+}
+
+struct Config{
+    inpath: String,
+    outpath: String,
+    min_qual: f64,
+    min_len: usize,
+    max_len: usize,
+    phred_offset: u8,
+}
+
+impl Config {
+    fn new() -> Config {
+        let arguments: Vec<String> = env::args().collect();
+        if arguments.len() < 6 + 1 {
+            eprintln!("Usage: nanofilt <inpath> <outpath> <minqual> <minlength> <maxlength> <phred_offset>");
             exit(1);
         }
-    };
-    if let Err(e) = foo(&path) {
-        eprintln!("Error parsing file: {}\n{}", &path, e);
-    };
+        let min_qual: f64 = parse_num::<f64>(&arguments[3]);
+        let min_len: usize = parse_num::<usize>(&arguments[4]);
+        let max_len: usize = parse_num::<usize>(&arguments[5]);
+        let phred_offset: u8 = parse_num::<u8>(&arguments[6]);
+        Config{inpath: arguments[1].clone(), outpath: arguments[2].clone(),
+        min_qual, min_len, max_len, phred_offset}
+    }
+}
+
+fn parse_num<T: FromStr>(str: &str) -> T 
+where <T as FromStr>::Err: std::fmt::Display {
+    match str.parse::<T>() {
+        Ok(n) => n,
+        Err(e) => {
+            eprintln!("Cannot parse argument {} as the right number type: {}", str, e);
+            exit(1)
+        }
+    }
 }
